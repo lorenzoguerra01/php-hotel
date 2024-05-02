f3518c5237634859d1b0b8a3451a77594368c69f
lorenzoguerra01 <guerra.lo11-lg@gmail.com>
2024-05-02 16:34:54 +0200

    add scaffolding

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    include __DIR__ . '/Views/header.php';    
+?>  
+
+  <div id="app" class="ms_screen text-center">
+        <h1>PHP Hotel</h1>
+        <div class="card ms_card">
+            <h2>Hotel</h2>
+        </div>
+    </div>
+</body>
+
+</html>
